893acb6ce381d67bdb64541d864af70525b93eac
Torsten Walter <twalter@deloitte.de>
2016-12-14 13:44:16 +0100

    new generated maven project

---
+++ src/main/java/de/torstenwalter/examples/App.java
@@ -0,0 +1,13 @@
+package de.torstenwalter.examples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
